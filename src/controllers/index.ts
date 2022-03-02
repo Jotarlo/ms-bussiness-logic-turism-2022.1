c3f42968d6bb1411420422bd3a2ca74f3a6f6b85
Jeferson Arango <jeferson.arango@ucaldas.edu.co>
2022-03-02 09:32:52 -0500

    Controllers for amusement, city and region

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,7 @@
-export * from './ping.controller';
 export * from './city-region.controller';
+export * from './city.controller';
+export * from './ping.controller';
 export * from './region-city.controller';
+export * from './region.controller';
+
+export * from './amusement.controller';
